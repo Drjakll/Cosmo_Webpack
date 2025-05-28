57aa93015bdcaf0c1424b960f228ffbde79f6943
Justin <dr_kimsora@yahoo.com>
2025-05-28 11:10:19 -0700

    update

---
+++ Development/Client/View_Templates/Profile/Components/Posts/Single_Post/single_post.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './single_post.less';
+
+class Single_Post extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+
+        return <div id="single-post">
+
+        </div>;
+    }
+}
+
+export default Single_Post;
--- Development/Client/View_Templates/Profile/Components/Posts/posts.js
+++ Development/Client/View_Templates/Profile/Components/Posts/posts.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Single_Post from './Single_Post/single_post.js';
 import './posts.less';
 
 class Posts extends Component {
@@ -9,60 +10,50 @@ class Posts extends Component {
 
         Posts.contextType = window.Context;
 
+        this.state = {
 
+        };
     }
     
-    
+
+
     render() {
 
         let { Calendar } = this.context;
         
         return (
             <div id="posts">
-                
+
                 <div id="top">
-                
+
                     <div id="calendar-wrapper">
-                        
+
                         <div id="date-display-clickable">
                             May 27, 2025
                         </div>
-                        
+
                         <div id="calendar-popup">
-                        
-                            <Calendar year={2025} 
-                                    month={5} 
-                                    date={27} 
-                                    callback_left={(e) => { }} 
-                                    callback_right={(e) => { }} 
-                                    date_properties={[
-                                            {date: 27,
-                                             style: {border: "darkorange solid 3px", 
-                                                     color: "black",
-                                                     borderRadius: "50px",
-                                                     padding: "5px"
-                                                    },
-                                             callback: ()=>{alert("Hi!!");},
-                                             popup: <div
-                                                style={
-                                                    {
-                                                        position: "absolute",
-                                                        bottom: "100%",
-                                                        width: "100px",
-                                                        height: "50px",
-                                                        backgroundColor: "black",
-                                                        color: "white"
-                                                    }
-                                                }
-                                             >Stupid!</div>
-                                            }
-                                        ]} 
-                                    />
-                        
+
+                            <Calendar year={2025}
+                                month={5}
+                                date={27}
+                                callback_left={(e) => { }}
+                                callback_right={(e) => { }}
+                                date_properties={[]}
+                            />
+
                         </div>
-                    </div>   
-                    
+
+                    </div>
+
                 </div>
+
+                <div id="bottom">
+
+                    
+
+                </div>
+
             </div>
         );
     }
