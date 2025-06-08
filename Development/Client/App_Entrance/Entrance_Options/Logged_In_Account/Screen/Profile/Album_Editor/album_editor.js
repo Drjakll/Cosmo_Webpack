97017a4d5c09b87001acb4b00cde188474848811
Drjakll <dr_kimsora@yahoo.com>
2025-06-07 22:16:44 -0700

    small update

---
+++ Development/Client/App_Entrance/Entrance_Options/Logged_In_Account/Screen/Profile/Album_Editor/album_editor.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Album_Editor extends Component {
+    
+    constructor(props){
+        
+        super(props);
+        
+        
+    }
+    
+    render(){
+        
+        return <div id="album-editor">
+        
+            
+        
+        </div>;
+    }
+    
+}
