fe59f7cc6d546ef85fdafe6d59bc265ac8a80bb5
Justin <dr_kimsora@yahoo.com>
2025-12-31 21:00:37 -0800

    small update

--- Development/Client/Universal_Components/Comments_Container/Comment_Input/comment_input.js
+++ Development/Client/Universal_Components/Comments_Container/Comment_Input/comment_input.js
@@ -3,6 +3,8 @@ import './comment_input.less';
 
 class Comment_Input extends Component {
 
+    text = ""
+
     constructor(props){
 
         super(props);
@@ -10,11 +12,44 @@ class Comment_Input extends Component {
         
     }
 
+    Submit_Message = ()=>{
+
+    }
+
     render(){
 
         return (<div id="comment-input-wrapper">
 
+            <div id="input-wrapper">
+
+                <div id="textarea-wrapper">
+
+                    <textarea onChange={(e)=>{ this.text = e.target.value; }} 
+                        onKeyDown={(e)=>{
+
+                            if(e.key !== "Enter"){
+                                return;
+                            }
+
+                            e.target.value = "";
+
+                            this.Submit_Message();
+
+                        }}></textarea>
+
+                </div>
+
+                <div id="send-button-wrapper">
+
+                    <div id="send-button" onClick={this.Submit_Message}>
+
+                        Send
                         
+                    </div>
+
+                </div>
+
+            </div>
 
         </div>);
     }
